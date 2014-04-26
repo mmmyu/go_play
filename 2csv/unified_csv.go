760a0f4f593c493998f80ab60a4bff10578a4e1f
Marco Yu <yumf@silly.localdomain>
2014-04-26 13:17:06 -0700

    Added classifiers

--- 2csv/unified_csv.go
+++ 2csv/unified_csv.go
@@ -16,10 +16,68 @@ func usage() {
 }
 
 const (
-	out_date   = iota
-	out_amount = iota
-	out_desc   = iota
+	out_date        = iota
+	out_amount      = iota
+	out_desc        = iota
+	out_links       = iota
+	out_comment     = iota
+	out_payment     = iota
+	out_cash        = iota
+	out_food        = iota
+	out_restaurant  = iota
+	out_gas         = iota
+	out_car         = iota
+	out_online      = iota
+	out_drugs       = iota
+	out_school      = iota
+	out_kids        = iota
+	out_target      = iota
+	out_walmart     = iota
+	out_costco      = iota
+	out_store       = iota
+	out_fashion     = iota
+	out_home_improv = iota
+	out_electronics = iota
+	out_doctors     = iota
+	out_utilities   = iota
+	out_hoa         = iota
+	out_misc        = iota
+	out_travel      = iota
+	out_tax         = iota
+	out_mortgage    = iota
+	out_num_fields  = iota
 )
+
+var jump_table = [out_num_fields]func(r []string){
+	nil, // date
+	nil, // amount
+	nil, // desc
+	nil, // links
+	nil, // comment
+	classify_payment,
+	nil, // cash
+	classify_food,
+	classify_restaurant,
+	classify_gas,
+	nil, // car
+	classify_online,
+	nil, // drugs
+	classify_school,
+	nil, // kids
+	classify_target,
+	nil, // walmart
+	classify_costco,
+	nil, // store
+	nil, // fashion
+	classify_home_improv,
+	classify_electronics,
+	nil, // doctors
+	classify_utilities,
+	classify_hoa,
+	nil, // misc
+	nil, // travel/sports
+	classify_mortgage,
+}
 
 func convertDate(d string) string {
 	t, err := time.Parse("1/2/2006", d)
@@ -29,8 +87,118 @@ func convertDate(d string) string {
 	return t.Format("2006-01-02")
 }
 
+func has_string(a, b string) bool {
+	return strings.Contains(strings.ToLower(a),
+		strings.ToLower(b))
+}
+
+func classify_mortgage(r []string) {
+	if has_string(r[out_desc], "PROVIDENT FUND") ||
+		has_string(r[out_desc], "CALIBER") {
+		r[out_school] = r[out_amount]
+	}
+}
+
+func classify_school(r []string) {
+	if has_string(r[out_desc], "TAEKWON") ||
+		has_string(r[out_desc], "TIFFANY'S DANCE") {
+		r[out_school] = r[out_amount]
+	}
+}
+
+func classify_hoa(r []string) {
+	if has_string(r[out_desc], "PARC METRO") {
+		r[out_hoa] = r[out_amount]
+	}
+}
+
+func classify_home_improv(r []string) {
+	if has_string(r[out_desc], "Home Depot") {
+		r[out_home_improv] = r[out_amount]
+	}
+}
+
+func classify_costco(r []string) {
+	if has_string(r[out_desc], "Costco WHSE") {
+		r[out_costco] = r[out_amount]
+	}
+}
+
+func classify_electronics(r []string) {
+	if has_string(r[out_desc], "www.newegg.com") {
+		r[out_electronics] = r[out_amount]
+	}
+}
+
+func classify_target(r []string) {
+	if has_string(r[out_desc], "target") {
+		r[out_target] = r[out_amount]
+	}
+}
+
+func classify_utilities(r []string) {
+	if has_string(r[out_desc], "AT&T*BILL") ||
+		has_string(r[out_desc], "OOMAINC") ||
+		has_string(r[out_desc], "PLEASANTON WATER") ||
+		has_string(r[out_desc], "COMCAST") {
+		r[out_utilities] = r[out_amount]
+	}
+}
+
+func classify_online(r []string) {
+	if has_string(r[out_desc], "netflix.com") {
+		r[out_online] = r[out_amount]
+	}
+}
+
+func classify_gas(r []string) {
+	if has_string(r[out_desc], "Chevron") ||
+		has_string(r[out_desc], "Costco gas") ||
+		has_string(r[out_desc], "Union 76") ||
+		has_string(r[out_desc], "76 fuel") ||
+		has_string(r[out_desc], "Shell Oil") {
+		r[out_gas] = r[out_amount]
+	}
+}
+
+func classify_restaurant(r []string) {
+	if has_string(r[out_desc], "Starbucks") ||
+		has_string(r[out_desc], "Tully") ||
+		has_string(r[out_desc], "Peet") {
+		r[out_restaurant] = r[out_amount]
+	}
+}
+
+func classify_payment(r []string) {
+	if has_string(r[out_desc], "PAYMENT") {
+		v, err := strconv.ParseFloat(r[out_amount], 32)
+		if err == nil && v >= 0.0 {
+			r[out_payment] = r[out_amount]
+		}
+	}
+}
+
+func classify_food(r []string) {
+	if has_string(r[out_desc], "99 RANCH") ||
+		has_string(r[out_desc], "SAFEWAY") ||
+		has_string(r[out_desc], "FOOD EXPRESS") ||
+		has_string(r[out_desc], "WHOLE FOODS") ||
+		has_string(r[out_desc], "TRADER JOE") ||
+		has_string(r[out_desc], "RALEY'S") {
+		r[out_food] = r[out_amount]
+	}
+}
+
+func classify(record []string) {
+	for i := 0; i < out_num_fields; i++ {
+		if record[i] == "" && jump_table[i] != nil {
+			jump_table[i](record)
+		}
+	}
+}
+
 func convertWFB(record []string) []string {
-	out := make([]string, 7)
+	out := make([]string, out_num_fields)
 	amt, err := strconv.ParseFloat(record[1], 32)
 	if err != nil {
 		panic(err)
@@ -53,7 +221,7 @@ func convertWFB(record []string) []string {
 }
 
 func convertAmex(record []string) []string {
-	out := make([]string, 3)
+	out := make([]string, out_num_fields)
 	out[out_date] = convertDate(record[0])
 	out[out_amount] = record[2]
 	out[out_desc] = record[3] + " " + record[4]
@@ -61,7 +229,7 @@ func convertAmex(record []string) []string {
 }
 
 func convertCap1(record []string) []string {
-	out := make([]string, 3)
+	out := make([]string, out_num_fields)
 	out[out_date] = convertDate(record[0])
 	if out[out_date] == "" {
 		return out
@@ -81,7 +249,7 @@ func convertCap1(record []string) []string {
 }
 
 func convertCiti(record []string) []string {
-	out := make([]string, 3)
+	out := make([]string, out_num_fields)
 	out[out_date] = convertDate(record[0])
 	amt, err := strconv.ParseFloat(record[1][1:], 32)
 	if err != nil {
@@ -93,7 +261,7 @@ func convertCiti(record []string) []string {
 }
 
 func convertChase(record []string) []string {
-	out := make([]string, 3)
+	out := make([]string, out_num_fields)
 	out[out_date] = convertDate(record[1])
 	if out[out_date] == "" {
 		return out
@@ -135,12 +303,18 @@ func guessFileType(fname string) (int, string) {
 
 func ftypeToEnum(ft string) (int, string) {
 	switch ft {
-	case "wfb": return is_wfb, ft
-	case "amex": return is_amex, ft
-	case "cap1": return is_cap1, ft
-	case "chase": return is_chase, ft
-	case "citi": return is_citi, ft
-	default: panic("Unknown file type " + ft)
+	case "wfb":
+		return is_wfb, ft
+	case "amex":
+		return is_amex, ft
+	case "cap1":
+		return is_cap1, ft
+	case "chase":
+		return is_chase, ft
+	case "citi":
+		return is_citi, ft
+	default:
+		panic("Unknown file type " + ft)
 	}
 }
 
@@ -177,6 +351,7 @@ func convert(ftype int, fin string, out_file *os.File) {
 			panic("Unknown file type " + string(ftype))
 		}
 		if out[out_date] != "" {
+			classify(out)
 			output_records = append(output_records, out)
 		}
 	}
--- hello/helloworld.go
+++ hello/helloworld.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"net"
+)
 
 func main() {
-    fmt.Printf("hello, world\n")
+	fmt.Println("Welcome to the playground!")
+
+	conn, err := net.Dial("tcp", "google.com:80")
+	if err != nil {
+		return
+	}
+	fmt.Fprintf(conn, "GET / HTTP/1.0\r\n\r\n")
+	scanner := bufio.NewScanner(bufio.NewReader(conn))
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
 }
